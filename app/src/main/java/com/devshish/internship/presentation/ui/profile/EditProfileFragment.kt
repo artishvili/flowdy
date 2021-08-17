a772a50866e98e8ad2df18e6163c6894a5d89d9d
ArtemShishko <artem.shishko011@gmail.com>
2021-08-17 14:41:36 +0300

    fix: changed edit profile screen

--- app/src/main/java/com/devshish/internship/presentation/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/devshish/internship/presentation/ui/profile/EditProfileFragment.kt
@@ -1,21 +1,6 @@
 package com.devshish.internship.presentation.ui.profile
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.devshish.internship.R
-import com.devshish.internship.databinding.FragmentEditProfileBinding
 
-class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentEditProfileBinding.bind(view)
-
-        binding.btnBack.setOnClickListener {
-            val action = EditProfileFragmentDirections.actionEditProfileFragmentToProfileFragment()
-            findNavController().navigate(action)
-        }
-    }
-}
+class EditProfileFragment : Fragment(R.layout.fragment_edit_profile)
