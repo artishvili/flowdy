6ede51e802b8dc9b949d72b062a56e296652734d
ArtemShishko <artem.shishko011@gmail.com>
2021-08-30 16:42:34 +0300

    fix: rebased git changes & minor changes

--- app/src/main/java/com/devshish/internship/presentation/ui/likedalbums/LikedAlbumsFragment.kt
+++ app/src/main/java/com/devshish/internship/presentation/ui/likedalbums/LikedAlbumsFragment.kt
@@ -6,16 +6,17 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devshish.internship.R
 import com.devshish.internship.databinding.FragmentLikedAlbumsBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LikedAlbumsFragment : Fragment(R.layout.fragment_liked_albums) {
 
+    private val binding by viewBinding(FragmentLikedAlbumsBinding::bind)
     private val viewModel: AlbumsViewModel by viewModel()
     private val itemAlbumAdapter = ItemAlbumAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentLikedAlbumsBinding.bind(view)
 
         binding.rvAlbums.apply {
             adapter = itemAlbumAdapter
