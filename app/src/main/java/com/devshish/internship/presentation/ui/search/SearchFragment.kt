7ecd8c1347efbe6b67ef65be2cf99df76ed35a26
ArtemShishko <artem.shishko011@gmail.com>
2021-09-23 15:48:42 +0300

    feat[start]: search screen

--- app/src/main/java/com/devshish/internship/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/devshish/internship/presentation/ui/search/SearchFragment.kt
@@ -1,6 +1,32 @@
 package com.devshish.internship.presentation.ui.search
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.devshish.internship.R
+import com.devshish.internship.databinding.FragmentSearchBinding
+import com.devshish.internship.presentation.ui.utils.onQueryTextChanged
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
-class SearchFragment : Fragment(R.layout.fragment_search)
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+    private val binding by viewBinding(FragmentSearchBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.itemSearch)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            // TODO
+        }
+    }
+}
