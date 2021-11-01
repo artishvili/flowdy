8d0730940cce489dcfac0e4475638b8468794a67
ArtemShishko <artem.shishko011@gmail.com>
2021-11-01 16:03:58 +0300

    fix: pull request fixes

--- app/src/main/java/com/devshish/internship/presentation/service/player/PlayerNotificationManager.kt
+++ app/src/main/java/com/devshish/internship/presentation/service/player/PlayerNotificationManager.kt
@@ -1,5 +1,6 @@
 package com.devshish.internship.presentation.service.player
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -50,19 +51,18 @@ class PlayerNotificationManager(
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance).apply {
-                description = NOTIFICATION_CHANNEL_DESCRIPTION
-            }
+            val channel =
+                NotificationChannel(CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance).apply {
+                    description = NOTIFICATION_CHANNEL_DESCRIPTION
+                }
             val notificationManager: NotificationManager =
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
     }
 
-    private fun showNotification() {
-        with(NotificationManagerCompat.from(context)) {
-            notify(NOTIFICATION_ID, notificationBuilder.build())
-        }
+    private fun Notification.show() {
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, this)
     }
 
     fun onFirstPlay(song: Song?) {
@@ -71,21 +71,24 @@ class PlayerNotificationManager(
             .setContentText(song?.artist)
             .clearActions()
             .addAction(setActionAttr(R.drawable.exo_icon_pause))
-        showNotification()
+            .build()
+            .show()
     }
 
     fun onPlay() {
         notificationBuilder
             .clearActions()
             .addAction(setActionAttr(R.drawable.exo_icon_pause))
-        showNotification()
+            .build()
+            .show()
     }
 
     fun onPause() {
         notificationBuilder
             .clearActions()
             .addAction(setActionAttr(R.drawable.exo_icon_play))
-        showNotification()
+            .build()
+            .show()
     }
 
     private fun setIntent(action: Long): PendingIntent =
